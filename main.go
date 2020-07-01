90795a34d1dd72a5c28f5abc7c194e7b6c64b320
Alex <alexhutman@gmail.com>
2020-07-01 17:34:36 -0400

    Boilerplate lambda code

--- main.go
+++ main.go
@@ -4,18 +4,23 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
-	/*
 	"github.com/aws/aws-lambda-go/lambda"
+	/*
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	*/
 )
 
+func main() {
+	lambda.Start(Handler)
+}
+
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		fmt.Println("Cloudwatch not working.")
-		return events.APIGatewayProxyResponse{
-			StatusCode: 200,
-			Body: "Hello",
+	fmt.Println("Cloudwatch not working.")
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body: "Hello",
 	}, nil
 }

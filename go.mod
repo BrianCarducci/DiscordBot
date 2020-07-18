0b6765d7a6bb23398b3f5b5f981dd5eaede3b59f
Alex <alexhutman@gmail.com>
2020-07-18 03:13:35 -0400

    added discordgo dependency and added simple ping/pong msgs
    good thing is we cant use this lambda since discord doesn't have outgoing webhooks :D

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/BrianCarducci/DiscordBot
 
 go 1.14
 
-require github.com/aws/aws-lambda-go v1.17.0
+require (
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/bwmarrin/discordgo v0.21.1
+)
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bwmarrin/discordgo"
 	/*
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -17,10 +19,50 @@ func main() {
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Cloudwatch not working.")
+	token := os.Getenv("TOKEN")
 
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return createResponse(403, "Error creating Discord session: " + err.Error(), err)
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	discord.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		return createResponse(403, "Error opening connection: " + err.Error(), err)
+	}
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+
+	return createResponse(200, "This is working lol", nil)
+}
+
+func createResponse(code int, body string, err error) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body: "Hello",
-	}, nil
+		StatusCode: code,
+		Body: body,
+	}, err
+}
+
+// Using code from https://github.com/bwmarrin/discordgo/blob/master/examples/pingpong/main.go for testing
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }

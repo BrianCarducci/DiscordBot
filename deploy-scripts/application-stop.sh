f0e57ef327e8bb207cab57d70d56e0c0e7be9530
Alex <alexhutman@gmail.com>
2020-08-15 02:46:09 -0400

    only output to log after log dir is created

--- deploy-scripts/application-stop.sh
+++ deploy-scripts/application-stop.sh
@@ -16,13 +16,15 @@ fi
 # create logs directory
 mkdir -p $LOGS_DIR
 
-# kill running bot
-PROCS=$(ps -ax | grep './main' | grep -v 'grep' | awk '{ print $1 }')
-if [ -z "$PROCS" ]
-then
-  echo "No running DiscordBots."
-else
-  echo "Killing the following processes:"
-  ps -p $PROCS
-  kill -9 $PROCS
-fi
+{
+  # kill running bot
+  PROCS=$(ps -ax | grep './main' | grep -v 'grep' | awk '{ print $1 }')
+  if [ -z "$PROCS" ]
+  then
+    echo "No running DiscordBots."
+  else
+    echo "Killing the following processes:"
+    ps -p $PROCS
+    kill -9 $PROCS
+  fi
+} >> $LOGS_DIR/application-stop.log 2>&1 

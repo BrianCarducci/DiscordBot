274159f4fba01c95750540b6cf2ca602864b163b
Alex <alexhutman@gmail.com>
2020-08-15 02:23:26 -0400

    removing log for now

--- deploy-scripts/application-stop.sh
+++ deploy-scripts/application-stop.sh
@@ -5,26 +5,24 @@ set -e
 export BOT_DIR=/home/ubuntu/DiscordBot
 export LOGS_DIR=$BOT_DIR/logs
 
-{
-  # rm DiscordBot directory contents if they exist
-  if [ -d $BOT_DIR ]
-  then
-  rm -rf $BOT_DIR/*
-  else
-  mkdir -p $BOT_DIR
-  fi
+# rm DiscordBot directory contents if they exist
+if [ -d $BOT_DIR ]
+then
+rm -rf $BOT_DIR/*
+else
+mkdir -p $BOT_DIR
+fi
 
-  # create logs directory
-  mkdir -p $LOGS_DIR
+# create logs directory
+mkdir -p $LOGS_DIR
 
-  # kill running bot
-  PROCS=$(ps -ax | grep './main' | grep -v 'grep' | awk '{ print $1 }')
-  if [ -z "$PROCS" ]
-  then
-    echo "No running DiscordBots."
-  else
-    echo "Killing the following processes:"
-    ps -p $PROCS
-    kill -9 $PROCS
-  fi
-} >> $LOGS_DIR/application-stop.log 2>&1 
+# kill running bot
+PROCS=$(ps -ax | grep './main' | grep -v 'grep' | awk '{ print $1 }')
+if [ -z "$PROCS" ]
+then
+  echo "No running DiscordBots."
+else
+  echo "Killing the following processes:"
+  ps -p $PROCS
+  kill -9 $PROCS
+fi

4a4aabe5698124b9e1f1d9e0b8752987ba39e1d5
Alex <alexhutman@gmail.com>
2020-08-20 20:00:03 -0400

    secretsmanager get func returns errs if they occur

--- main.go
+++ main.go
@@ -50,28 +50,28 @@ func getAWSSecrets(secretKeys []string) (map[string]string, error) {
 			switch aerr.Code() {
 				case secretsmanager.ErrCodeDecryptionFailure:
 				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+				return nil, errors.New(secretsmanager.ErrCodeDecryptionFailure + ": " + aerr.Error())
 
 				case secretsmanager.ErrCodeInternalServiceError:
 				// An error occurred on the server side.
-				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+				return nil, errors.New(secretsmanager.ErrCodeInternalServiceError + ": " + aerr.Error())
 
 				case secretsmanager.ErrCodeInvalidParameterException:
 				// You provided an invalid value for a parameter.
-				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+				return nil, errors.New(secretsmanager.ErrCodeInvalidParameterException + ": " + aerr.Error())
 
 				case secretsmanager.ErrCodeInvalidRequestException:
 				// You provided a parameter value that is not valid for the current state of the resource.
-				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+				return nil, errors.New(secretsmanager.ErrCodeInvalidRequestException + ": " + aerr.Error())
 
 				case secretsmanager.ErrCodeResourceNotFoundException:
 				// We can't find the resource that you asked for.
-				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+				return nil, errors.New(secretsmanager.ErrCodeResourceNotFoundException + ": " + aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			fmt.Println(err.Error())
+			return nil, err
 		}
 	}
 

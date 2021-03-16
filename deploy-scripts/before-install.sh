fc812a4e500ff7394186679fde14a7f1c00e6dfd
Alex <alexhutman@gmail.com>
2021-03-16 19:21:23 -0400

    use inline dependencies instead of external files
    since its beforeinstall, the requirements files aren't present yet

--- deploy-scripts/before-install.sh
+++ deploy-scripts/before-install.sh
@@ -5,11 +5,25 @@ export LOGS_DIR=$BOT_DIR/logs
 
 # Install necessary apt utilities
 echo "Installing apt packages" >> $LOGS_DIR/before-install.log
-grep -vE '^#' $BOT_DIR/deploy-scripts/apt-utils.txt | xargs sudo apt-get -y install >> $LOGS_DIR/before-install.log 2>&1
+{
+cat <<-EOF
+# Install gcc for gopus library
+build-essential
+EOF
+} | grep -vE '^#' | xargs sudo apt-get -y install >> $LOGS_DIR/before-install.log 2>&1
 
 # Install necessary snap utilities
 printf "\nInstalling snap packages\n" >> $LOGS_DIR/before-install.log
+{
+cat <<-EOF
+#--channel=latest/stable go
+# Install ffmpeg to convert Polly audio sample rate to 48KHz
+ffmpeg
+EOF
+} | grep -vE '^#' |
+{
 while IFS= read -r line; do
 	echo "Attempting to install $line"
 	sudo snap install "$line"
-done < <(grep -vE '^#' $BOT_DIR/deploy-scripts/snap-utils.txt) >> $LOGS_DIR/before-install.log 2>&1
+done >> $LOGS_DIR/before-install.log 2>&1
+}

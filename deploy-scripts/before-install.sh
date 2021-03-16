4d5aa344b3a16d057ae5f963d4986db14d6801bb
Alex <alexhutman@gmail.com>
2021-03-16 18:17:22 -0400

    separated install of snap packages and apt packages

--- deploy-scripts/before-install.sh
+++ deploy-scripts/before-install.sh
@@ -3,5 +3,10 @@
 export BOT_DIR=/home/ubuntu/DiscordBot
 export LOGS_DIR=$BOT_DIR/logs
 
-# Install gcc for gopus library
-sudo apt-get -y install build-essential >> $LOGS_DIR/before-install.log 2>&1
+# Install necessary apt utilities
+grep -vE '^#' $BOT_DIR/deploy-scripts/apt-utils.txt | xargs sudo apt-get -y install >> $LOGS_DIR/before-install.log 2>&1
+
+# Install necessary snap utilities
+while IFS= read -r line; do
+	sudo snap install "$line" >> $LOGS_DIR/before-install.log 2>&1
+done < <(grep -vE '^#' $BOT_DIR/deploy-scripts/snap-utils.txt)

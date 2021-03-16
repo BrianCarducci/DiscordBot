eee0e5327db790529b6493422ad80bd5a8fc7e65
Alex <alexhutman@gmail.com>
2021-03-15 22:44:36 -0400

    install gcc for gopus

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -2,7 +2,11 @@
 
 export BOT_DIR=/home/ubuntu/DiscordBot
 export LOGS_DIR=$BOT_DIR/logs
+export CGO_CFLAGS="-O2"
 
 cd $BOT_DIR
+
+# Install gcc for gopus library
+sudo apt-get -y install build-essential
 
 /snap/bin/go build main.go >> $LOGS_DIR/after-install.log 2>&1

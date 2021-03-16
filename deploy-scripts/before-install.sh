28f2d29f4f8751533744d7b3ac0df354d0e003ae
Alex <alexhutman@gmail.com>
2021-03-16 00:32:53 -0400

    added chgrp as well

--- deploy-scripts/before-install.sh
+++ deploy-scripts/before-install.sh
@@ -6,6 +6,7 @@ export LOGS_DIR=$BOT_DIR/logs
 # Install gcc for gopus library
 sudo apt-get -y install build-essential >> $LOGS_DIR/before-install.log 2>&1
 
-# chown/chmod directories so ubuntu user can write logs
+# chown/chgrp/chmod DiscordBot directory so ubuntu user can write logs
 sudo chown -R ubuntu $BOT_DIR
+sudo chgrp -R ubuntu $BOT_DIR
 sudo chmod -R 744 $BOT_DIR

7a2cea78204904723baa036dff3aec75713dd8a8
Alex <alexhutman@gmail.com>
2021-03-16 19:05:43 -0400

    added status messages to log

--- deploy-scripts/before-install.sh
+++ deploy-scripts/before-install.sh
@@ -4,11 +4,11 @@ export BOT_DIR=/home/ubuntu/DiscordBot
 export LOGS_DIR=$BOT_DIR/logs
 
 # Install necessary apt utilities
-echo "Installing apt packages"
+echo "Installing apt packages" >> $LOGS_DIR/before-install.log
 grep -vE '^#' $BOT_DIR/deploy-scripts/apt-utils.txt | xargs sudo apt-get -y install >> $LOGS_DIR/before-install.log 2>&1
 
 # Install necessary snap utilities
-printf "\nInstalling snap packages\n"
+printf "\nInstalling snap packages\n" >> $LOGS_DIR/before-install.log
 while IFS= read -r line; do
 	echo "Attempting to install $line"
 	sudo snap install "$line"

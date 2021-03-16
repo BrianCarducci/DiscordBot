a9f02b54299910841e795ff92142e27881009776
Alex <alexhutman@gmail.com>
2021-03-16 00:37:40 -0400

    application start run as ubuntu user, moved ch{grp,mod,own} to after-install

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -4,6 +4,11 @@ export BOT_DIR=/home/ubuntu/DiscordBot
 export LOGS_DIR=$BOT_DIR/logs
 export CGO_CFLAGS="-O2"
 
+# chown/chgrp/chmod DiscordBot directory so ubuntu user can write logs
+sudo chown -R ubuntu $BOT_DIR
+sudo chgrp -R ubuntu $BOT_DIR
+sudo chmod -R 744 $BOT_DIR
+
 cd $BOT_DIR
 
 /snap/bin/go build main.go >> $LOGS_DIR/after-install.log 2>&1
